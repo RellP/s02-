4016079eee64e85ef9087777b871d3fd939cdb88
RellP <91113644+RellP@users.noreply.github.com>
2022-09-25 21:23:43 +0800

    Update course.js

--- s02-Panganiban/course.js
+++ s02-Panganiban/course.js
@@ -1,17 +1,17 @@
 let course1 = {
     "_id" : ObjectId("631085a3b92ebab68a3b7dba"),
-    "name" : "Ice",
+    "name" : "Rell",
     "description" : "Student",
     "price" : 10000,
     "slots" : 50,
     "schedule" : "Monday",
-    "instructor" : "Mr. Habitan",
+    "instructor" : "Sir. Rell",
     "isActive" : "Yes",
     "enrollees" : [ 
         {
             "id" : "Enrollment1",
             "userId" : "63108322b92ebab68a3b7d07",
-            "userName(optional)" : "Ice",
+            "userName(optional)" : "Rell",
             "isPaid" : "yes",
             "dateEnrolled" : "09/01/2022"
         }
@@ -20,18 +20,18 @@ let course1 = {
 
 let course2 = {
     "_id" : ObjectId("63108843b92ebab68a3b7ea6"),
-    "name" : "Isaiah",
+    "name" : "Stephen",
     "description" : "Student",
     "price" : 10000,
     "slots" : 50,
     "schedule" : "Monday",
-    "instructor" : "Mr. Habitan",
+    "instructor" : "Sir. Panganiban",
     "isActive" : "Yes",
     "enrollees" : [ 
         {
             "id" : "Enrollment1",
             "userId" : "631087c3b92ebab68a3b7e7f",
-            "userName(optional)" : "Isaiah",
+            "userName(optional)" : "Rell",
             "isPaid" : "yes",
             "dateEnrolled" : "09/01/2022"
         }
