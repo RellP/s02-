7c41beeeb21ca056d72f0be6f816d0ec0a03404c
RellP <91113644+RellP@users.noreply.github.com>
2022-09-25 21:58:57 +0800

    Update course.js

--- s02-Panganiban/course.js
+++ s02-Panganiban/course.js
@@ -25,13 +25,13 @@ let course2 = {
     "price" : 10000,
     "slots" : 50,
     "schedule" : "Monday",
-    "instructor" : "Sir. Panganiban",
+    "instructor" : "Sir. Stephen",
     "isActive" : "Yes",
     "enrollees" : [ 
         {
             "id" : "Enrollment1",
             "userId" : "631087c3b92ebab68a3b7e7f",
-            "userName(optional)" : "Rell",
+            "userName(optional)" : "Stephen",
             "isPaid" : "yes",
             "dateEnrolled" : "09/01/2022"
         }
